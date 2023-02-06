b24567b144ba68f9b8bd64ec6b1f58bbe1b29992
Wangerion <wavesvibe@gmail.com>
2023-02-06 10:21:34 +0200

    Changed how '/' work.

--- nfsbckpscript.sh
+++ nfsbckpscript.sh
@@ -3,25 +3,27 @@
 truncate -s 0 errors.log
 current_date=$(date +%Y-%m-%d)
 nfs_var=( $(mount -l | grep nfs | awk '{print $3}') )
-#nfs_var=("/mnt/dba" "/dba2")
+#nfs_var=("/dba" "/dba2" "/backup")
 declare -p nfs_var
 
 #If the backup mountpoint is mounted we can start the rsync process
-if mountpoint /backup;
+if mountpoint -q /backup;
 then
     #Iterating over the mount points in the list provided.
     for value in "${nfs_var[@]}"
     do
-        rsync -au --exclude=/backup $value /backup/$value
-        #if the last exit code was not eq to 0 then try again the rsync process
-        if [ "$?" -ne "0" ]
+        if [[ $value != /backup ]]
         then
-            rsync -avu --exclude=/backup $value /backup/$value 2>> errors.log
-            echo "Script ran once more for $value" >> errors.log
+            rsync -auP $value/ /backup/$value
+            #if the last exit code was not eq to 0 then try again the rsync process
+            if [ "$?" -ne "0" ]
+            then
+                rsync -avuP $value/ /backup/$value 2>> errors.log
+                echo "Script ran once more for $value" >> errors.log
+            fi
         fi
     done
 else
     mountpoint /mnt/backup &>> errors.log
 fi
-
 echo "Kindly look at the attached" | mail -s "NFSbackup $current_date" user@example.com -A errors.log
